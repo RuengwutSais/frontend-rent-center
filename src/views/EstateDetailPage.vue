d009161fa97b9617e1d2ce71cc56105e40731084
panupong.shippop <phanupong.shippop@gmail.com>
2023-05-08 00:12:28 +0700

    fix bug

--- src/views/EstateDetailPage.vue
+++ src/views/EstateDetailPage.vue
@@ -414,7 +414,7 @@ export default {
     },
     isUserProfileEqual() {
       const user = JSON.parse(localStorage.getItem("profiles"));
-      return this.profile.user_id === user.user_id;
+      return user ? this.profile.user_id === user.user_id : true; 
     },
   },
   methods: {
